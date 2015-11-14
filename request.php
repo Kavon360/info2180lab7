1e1f4660c7b68d0ecc041b558a12a27ee6edddf7
Kavon360 <Kavon_Campbell@yahoo.com>
2015-11-13 20:09:08 -0600

    lab7
    lab7

---
+++ request.php
@@ -0,0 +1,29 @@
+<?php
+
+//ForPHP5andup
+
+#acceptaterm(keyword)
+
+#respondwithavalue
+
+$query=$_REQUEST['q'];
+
+$naseberry=[
+
+"definition"=>"a statement of the exact meaning of a word, especially in a
+
+dictionary.",
+
+"bar"=>"a place that sells alcholic beverages",
+
+"ajax"=>"technique which involves the use  ofjavascript and xml"
+
+];
+
+print$query;
+
+print"<br>";
+
+print($naseberry[$query]);
+
+?>
